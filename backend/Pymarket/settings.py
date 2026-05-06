ae9c467c8901133e5768b7d74c76030d96c8b412
abasiofon-sendan <abasiofon135@gmail.com>
2026-05-06 17:43:18 +0100

    Add created_at field to topupmodel and update Redis host configuration

--- backend/Pymarket/settings.py
+++ backend/Pymarket/settings.py
@@ -112,7 +112,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [REDIS_URL],
+            "hosts": ["redis://127.0.0.1:6379"],
             # "hosts": [('localhost', '6379')]
         }
     },
